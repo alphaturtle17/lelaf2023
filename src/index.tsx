cf815c1c2a0fe322021ac3eec15fa2035f2fd357
Thor Long <thorlong17@outlook.com>
2023-11-11 14:16:57 -0500

    react app

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from "react-dom";
+
+const Hello = () => <h1>Hello from React</h1>;
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
