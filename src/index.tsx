5bb8bb0462a3fd193346971d181b9b3989f8c452
Thor Long <thorlong17@outlook.com>
2023-11-11 15:18:58 -0500

    map centers at allentown

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from "react-dom";
-import "./style.css"
 
 const Hello = () => <h1>Hello from React</h1>;
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+//ReactDOM.render(<Hello />, document.getElementById('app'));
